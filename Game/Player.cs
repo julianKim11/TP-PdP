ca5f4eb27f8c4769e23c0100b1fed7306ebdf00e
julianKim11 <102937334+julianKim11@users.noreply.github.com>
2023-04-30 13:52:17 -0300

    Add files via upload

---
+++ Game/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class Player
+    {
+        private string image;
+        private float posX, posY;
+        private float movementSpeed;
+        private float scaleX, scaleY;
+        private float angle;
+        private float pivotX, pivotY;
+
+        public Player(string image, float posX, float posY, float scaleX, float scaleY, float angle, float pivotX, float pivotY)
+        {
+            this.image = image;
+            this.posX = posX;
+            this.posY = posY;
+            this.scaleX = scaleX;
+            this.scaleY = scaleY;
+            this.angle = angle;
+            this.pivotX = pivotX;
+            this.pivotY = pivotY;
+
+            movementSpeed = 150f;
+        }
+        public void Update()
+        {
+            InputReading();
+        }
+        public void Render()
+        {
+            Engine.Draw("Textures/Player/NaveTop.png", posX, posY, scaleX, scaleY, angle, pivotX, pivotY);
+        }
+        private void InputReading()
+        {
+            if (Engine.GetKey(Keys.W)) posY -= movementSpeed * Program.deltaTime;
+            if (Engine.GetKey(Keys.S)) posY += movementSpeed * Program.deltaTime;
+            if (Engine.GetKey(Keys.D)) posX += movementSpeed * Program.deltaTime;
+            if (Engine.GetKey(Keys.A)) posX -= movementSpeed * Program.deltaTime;
+        }
+    }
+}
---
+++ Game/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game
+{
+    public class Program
+    {
+
+        private static DateTime startTime;
+        private static float lastFrame;
+        public static float deltaTime;
+
+        private static Player player = new Player("Textures/Player/NaveTop.png", 200, 100, 1f, 1f, 90, 32, 32);
+
+        static void Main(string[] args)
+        {
+            Engine.Initialize("Game", 1280, 720);
+
+            Start();
+
+            while(true)
+            {
+                Update();
+                Render();
+            }
+        }
+        private static void Start()
+        {
+            Engine.Debug("Start");
+            startTime = DateTime.Now;
+        }
+        private static void Update()
+        {
+            player.Update();
+            DeltaTimeCalculation();
+            
+        }
+        private static void Render()
+        {
+            Engine.Clear();
+            Engine.Draw("Textures/Background/FondoEspacial.png");
+            player.Render();
+            Engine.Show();
+        }
+        private static void DeltaTimeCalculation()
+        {
+            float currentFrame = (float)(DateTime.Now - startTime).TotalSeconds;
+            deltaTime = currentFrame - lastFrame;
+            lastFrame = currentFrame;
+        }
+    }
+}
